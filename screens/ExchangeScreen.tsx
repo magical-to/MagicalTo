63c799fa9ffc37aaa588bfbbac789f895766d1ca
JEONG-JIN-GYO <202021035@sangmyung.kr>
2025-03-07 04:21:35 +0900

    add new screens and update launcher icons

---
+++ screens/ExchangeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ExchangeScreen = () => {
+    return (
+        <View>
+        <Text>Exchange Screen</Text>
+        </View>
+    );
+};
+
+export default ExchangeScreen;
---
+++ screens/TagsScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const TagsScreen = () => {
+    return (
+        <View>
+        <Text>Tag Screen</Text>
+        </View>
+    );
+};
+
+export default TagsScreen;
